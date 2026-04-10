462ea81ebfbd03203e7fcf772e95f3c3dd9ac36c
nihadgojayev <nihadqocayev152@gmail.com>
2026-04-10 16:02:09 +0400

    Initial commit

---
+++ src/test/java/com/example/systemsanalysisfinalproject/SystemsAnalysisFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemsanalysisfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemsAnalysisFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
